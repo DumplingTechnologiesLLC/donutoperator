99f1016741555eb7edd736cd0a4c381b6b8edcf0
Pedro Del Moral Lopez <pdelmora@purdue.edu>
2018-09-13 19:34:08 -0400

    Pushing to live

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,26 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-# application = get_wsgi_application()
-
-
 import os
-import signal
 
-import sys
-import traceback
-
-import time
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+
+# import os
+# import signal
+
+# import sys
+# import traceback
+
+# import time
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
