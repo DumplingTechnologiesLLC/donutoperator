04c4d19e3c8ca77b150ab4e32f04f5c02d806ca6
Pedro Del Moral Lopez <pdelmora@purdue.edu>
2018-11-13 22:30:15 -0500

    Functionality at 100%

--- bodycams/urls.py
+++ bodycams/urls.py
@@ -8,6 +8,7 @@ app_name = "bodycams"
 urlpatterns = [
 	url(r'^bodycams$', BodycamIndexView.as_view(), name="bodycams"),
 	url(r'^bodycams/dashboard$', BodycamDashboard.as_view(), name="dashboard"),
+	url(r'^bodycams/dashboard/(?P<date>[0-9]+)$', BodycamDashboard.as_view(), name="dashboard-date"),
 	url(r'^bodycams/(?P<date>[0-9]+)$', BodycamIndexView.as_view(), name="date-index"),
 	url(r'^ajax/bodycam/submit$', BodycamSubmit.as_view(), name="bodycam-submit"),
 	url(r'^ajax/bodycam/edit$', BodycamEdit.as_view(), name="bodycam-edit"),
