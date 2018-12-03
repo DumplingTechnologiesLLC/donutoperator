2ff57561a7c9d733c38dc79ff33698758249826c
Pedro Del Moral Lopez <pdelmora@purdue.edu>
2018-12-02 20:02:00 -0500

    Heroku tool no longer necessary

--- config/settings.py
+++ config/settings.py
@@ -98,9 +98,7 @@ if LOCAL:
         }
     }
 else:
-    database_url = os.environ.get(
-        "DATABASE_URL",
-        'postgres://fuxlzxrhqroyhs:9b04496286c843805d34761d132b6c59169a752e58128300805b5e343e25016b@ec2-54-204-36-249.compute-1.amazonaws.com:5432/ddcdsj4jk8g5k7')
+    database_url = os.environ.get("DATABASE_URL", None)
     DATABASES = {
         'default': dj_database_url.config(
             default=database_url,
