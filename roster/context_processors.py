a9c9a7fe3f8e7b8b81f05ada982c4ee8d93130e7
Pedro Del Moral Lopez <pdelmora@purdue.edu>
2018-12-21 16:15:31 -0500

    navbar tips

--- roster/context_processors.py
+++ roster/context_processors.py
@@ -1,8 +1,11 @@
-from roster.models import Shooting, Tag
+from roster.models import Shooting, Tag, Tip
+
 
 def supply_basic_data(request):
     distinct_tags = Tag.objects.all().distinct()
+    number_of_tips = Tip.objects.all().count()
     return {
+        "number_of_tips": number_of_tips,
         "states": Shooting.STATE_CHOICES,
         "races": Shooting.RACE_CHOICES,
         "genders": Shooting.GENDER_CHOICES,
