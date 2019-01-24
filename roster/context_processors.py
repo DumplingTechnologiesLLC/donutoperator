fe238aafa117c91afc76965373754aa0d283c802
Pedro Del Moral Lopez <pdelmora@purdue.edu>
2019-01-23 20:48:51 -0500

    adding feedback

--- roster/context_processors.py
+++ roster/context_processors.py
@@ -1,10 +1,12 @@
-from roster.models import Shooting, Tag, Tip
+from roster.models import Shooting, Tag, Tip, Feedback
 
 
 def supply_basic_data(request):
     distinct_tags = Tag.objects.all().distinct()
     number_of_tips = Tip.objects.all().count()
+    number_of_feedbacks = Feedback.objects.all().count()
     return {
+    	"number_of_feedbacks": number_of_feedbacks,
         "number_of_tips": number_of_tips,
         "states": Shooting.STATE_CHOICES,
         "races": Shooting.RACE_CHOICES,
