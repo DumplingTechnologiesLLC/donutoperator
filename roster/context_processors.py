f6f202ea4736b0b623d0a0dddeb48b11cf1381f6
Pedro Del Moral Lopez <pdelmora@purdue.edu>
2019-03-15 19:26:46 -0400

    updating description

--- roster/context_processors.py
+++ roster/context_processors.py
@@ -6,10 +6,15 @@ def supply_basic_data(request):
     number_of_tips = Tip.objects.all().count()
     number_of_feedbacks = Feedback.objects.all().count()
     years = []
-    for x in range(2013, datetime.datetime.now().year + 1):
+    title_current_year = datetime.datetime.now().year
+    number_of_shootings_this_year = Shooting.objects.filter(
+        date__year=title_current_year).count()
+    for x in range(2013, title_current_year + 1):
         years.append(x)
     return {
+        "title_current_year": title_current_year,
         "valid_years": years,
+        "number_of_shootings_this_year": number_of_shootings_this_year,
     	"number_of_feedbacks": number_of_feedbacks,
         "number_of_tips": number_of_tips,
         "states": Shooting.STATE_CHOICES,
