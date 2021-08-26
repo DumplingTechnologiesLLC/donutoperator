3944578fa749747a5628f8edf3671e17763303c7
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2021-08-25 20:43:23 -0400

    requirements update

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,7 @@ else:
             ssl_require=True
         )
     }
-    DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+    # DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
